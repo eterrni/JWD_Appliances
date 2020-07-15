1914261827c950d2aa6e00258bc954c2f92ccb31
eterrni <61310114+eterrni@users.noreply.github.com>
2020-07-15 21:31:21 +0300

    First commit

---
+++ src/by/tc/task01/dao/ApplianceReader.java
@@ -0,0 +1,9 @@
+package by.tc.task01.dao;
+
+import by.tc.task01.dao.exception.DaoException;
+
+import java.util.List;
+
+public interface ApplianceReader {
+    List<String> loadGroup(String group) throws DaoException;
+}
---
+++ src/by/tc/task01/main/PrintApplianceInfo.java
@@ -0,0 +1,32 @@
+package by.tc.task01.main;
+
+import by.tc.task01.entity.*;
+
+public class PrintApplianceInfo {
+
+	private static final PrintCommandProvider provider = new PrintCommandProvider();
+
+	public static void print(Appliance appliance) {
+
+		PrintCommand command;
+
+		if (appliance instanceof Laptop) {
+			command = provider.takeCommand(Laptop.class.getSimpleName());
+		} else if (appliance instanceof Oven) {
+			command = provider.takeCommand(Oven.class.getSimpleName());
+		} else if (appliance instanceof Refrigerator) {
+			command = provider.takeCommand(Refrigerator.class.getSimpleName());
+		} else if (appliance instanceof Speakers) {
+			command = provider.takeCommand(Speakers.class.getSimpleName());
+		} else if (appliance instanceof TabletPC) {
+			command = provider.takeCommand(TabletPC.class.getSimpleName());
+		} else if (appliance instanceof VacuumCleaner) {
+			command = provider.takeCommand(VacuumCleaner.class.getSimpleName());
+		} else {
+			command = provider.takeCommand(PrintCommandProvider.NO_SUCH_APPLIANCE);
+		}
+
+		command.print(appliance);
+
+	}
+}
